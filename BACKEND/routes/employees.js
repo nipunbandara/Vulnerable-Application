06823ad3dc00efcae5684ccde9822db9c599f9aa
nipunbandara <nipun.bandara@outlook.com>
2021-08-19 23:52:53 +0530

    getall, update, delete, fetch one employee implemented

--- BACKEND/routes/employees.js
+++ BACKEND/routes/employees.js
@@ -26,7 +26,86 @@ router.route("/add").post((req ,res) => {
 
 } )
 
+//to get employee details when view is opened in html
+router.route("/").get((req, res)=> {
+    Employee.find().then((employees) => {
+        res.json(employees);
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+})
+
+//update one employee's data
+//:id is meant to get value as id after /update
+
+router.route("/update/:id").put(async (req, res) => {
+    //put is to get exist data and replace with update data
+
+    let userId = req.params.id; //fetch id from url
+    //d structure to fetch data from body
+    const {name, age, gender} = req.body;
+
+    const updateEmployee = {
+        name,
+        age,
+        gender
+    };
+    
+    const udpate = await Employee.findByIdAndUpdate(userId, updateEmployee)
+    //findOne is to used find object by other than id(pk)
+
+    .then(() => {
+        res.status(200).send({status: "user udpated", user : update}); // to send that update is successful. 200 - success code
+
+    })
+
+    .catch((err) => {
+        console.log(err); //1st error
+        res.status(500).send({status : "error with updating data", error: err.message}); //2nd method to notify error
+
+    })
+})
 
 
+//delete an employee
+
+router.route("/delete/:id").delete(async (req, res) => {
+    //send delete method instead post or put
+    let userId = req.params.id;
+    
+    await Employee.findByIdAndDelete(userId)
+    .then(() => {
+        res.status(200).send({status: "user deleted"});
+    })
+
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send({status : "error with deleting user", error : err.message});
+
+    })
+
+})
+
+
+//fetch one employee's data
+
+router.route("/get/:id").get(async (req, res) => {
+
+    let userId = req.params.id;
+    
+    //making user object to assign userdetail to send to front end
+    const user = await Employee.findById(userId)
+    .then(() => {
+        res.status(200).send({status : "user fetched", user : user}); //user is the object send here to pass user details
+
+    })
+
+    .catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status : "error with fetching user", error : error.message});
+
+    })
+})
 
 module.exports = router;
